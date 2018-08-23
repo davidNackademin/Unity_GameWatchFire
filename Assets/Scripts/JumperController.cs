61b401a6751a9f4bc7ffe2bfe7de562d167b8135
David Svensson <david@Admins-MacBook-Pro.local>
2018-08-23 10:51:54 +0200

    Jumper is moving

--- Assets/Scripts/JumperController.cs
+++ Assets/Scripts/JumperController.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 public class JumperController : MonoBehaviour {
 
     public List<Transform> positions = new List<Transform>();
+    int currentPosition = 0;
+    public float moveDelay = 0.5f;
+    float lastMoveTime;
 
 	// Use this for initialization
 	void Start () {
-        transform.position = positions[0].position;
+        transform.position = positions[currentPosition].position;
+        lastMoveTime = Time.time;
 
+        StartCoroutine(Move());
 	}
 	
+    IEnumerator Move() {
+        while(true) {
+            yield return new WaitForSeconds(moveDelay);
+            MoveToNextPosition();
+        }
+    }
+
+
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	//void Update () {
+        
+ //       if ( Time.time > lastMoveTime + moveDelay) {
+ //           MoveToNextPosition();
+
+ //       }
+	//}
+
+    void MoveToNextPosition() {
+        currentPosition++;
+
+        if (currentPosition >= positions.Count)
+            currentPosition = 0;
+
+        transform.position = positions[currentPosition].position;
+
+        lastMoveTime = Time.time;
+    }
+
+
 }
