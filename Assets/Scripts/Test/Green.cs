05ba258f3b78f7953dd46a5e093a75782a8ddd2e
David Svensson <david@Admins-MacBook-Pro.local>
2018-08-24 10:03:45 +0200

    TEst scene with collision is touching

--- Assets/Scripts/Test/Green.cs
+++ Assets/Scripts/Test/Green.cs
@@ -6,25 +6,56 @@ public class Green : MonoBehaviour {
 
     public GameObject redSquare;
 
+    Collider2D greenCollider;
+    Collider2D redCollider;
+
+    SpriteRenderer spriteRenderer;
+    Color startColor;
+
 
     private void Start()
     {
-        SpriteRenderer spriteRenderer =  GetComponent<SpriteRenderer>();
-       
-        spriteRenderer.color = Color.gray;
+        //SpriteRenderer spriteRenderer =  GetComponent<SpriteRenderer>();
+
+        //spriteRenderer.color = Color.gray;
 
 
-        transform.localScale = new Vector3(2f, 3f, 1f);
+        //transform.localScale = new Vector3(2f, 3f, 1f);
 
-      
-        SpriteRenderer redSpriteRenderer = redSquare.GetComponent<SpriteRenderer>();
 
-        redSpriteRenderer.color =Color.magenta;
+        //SpriteRenderer redSpriteRenderer = redSquare.GetComponent<SpriteRenderer>();
 
-        redSquare.transform.localScale = new Vector3(0.5f, 2.5f, 1f);
+        //redSpriteRenderer.color =Color.magenta;
 
+        //redSquare.transform.localScale = new Vector3(0.5f, 2.5f, 1f);
+
+
+        greenCollider = GetComponent<Collider2D>();
+        redCollider = redSquare.GetComponent<Collider2D>();
+
+        greenCollider.isTrigger = true;
+        redCollider.isTrigger = true;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color; 
+
+
+    }
+
+    private void Update()
+    {
+        if(redCollider.IsTouching(greenCollider)) {
+            spriteRenderer.color = Color.yellow; 
+        } else {
+            spriteRenderer.color = startColor;
+        }
 
 
 
     }
+
+
+
+
+
 }
