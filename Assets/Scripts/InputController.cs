208480f3382735d0c9b7813360bf9218e18945ee
David Svensson <david@Admins-MacBook-Pro-2.local>
2018-08-31 11:12:19 +0200

    Added touch input

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputController : MonoBehaviour {
+public class InputController : MonoBehaviour
+{
 
     public delegate void ButtonPressed();
     public static event ButtonPressed OnLeftPressed;
@@ -15,24 +16,31 @@ public class InputController : MonoBehaviour {
 
 #if UNITY_EDITOR
 
-        if(Input.GetMouseButtonDown(0)) {
-            
+        if (Input.GetMouseButtonDown(0))
+        {
+
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             if (OnLeftPressed != null && pos.x < -buttonMargin)
                 OnLeftPressed();
-            else if (OnRightPressed != null && pos.x > buttonMargin )
+            else if (OnRightPressed != null && pos.x > buttonMargin)
                 OnRightPressed();
-
-
-
         }
+#else
+        foreach(Touch touch in Input.touches ) {
+            if (touch.phase == TouchPhase.Began) {
+
+                Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+
+                if (OnLeftPressed != null && pos.x < -buttonMargin)
+                    OnLeftPressed();
+                else if (OnRightPressed != null && pos.x > buttonMargin)
+                    OnRightPressed();
+            }
+        }
+
+
 #endif
-
-
-
-
-
 
 
     }
